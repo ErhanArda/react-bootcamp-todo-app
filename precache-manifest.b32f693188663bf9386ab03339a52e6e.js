0792f5852de5b0f2201f4d79ed647d86f2878043
Erhan Arda <erhanarda7@gmail.com>
2020-05-01 00:07:19 +0300

    Updates

---
+++ precache-manifest.b32f693188663bf9386ab03339a52e6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16437ded8bd440b730550a55ab89f4de",
+    "url": "/react-bootcamp-todo-app/index.html"
+  },
+  {
+    "revision": "0b249c2669a9c753d258",
+    "url": "/react-bootcamp-todo-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6f80e1983f825cbab100",
+    "url": "/react-bootcamp-todo-app/static/js/2.b7f2fb11.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-bootcamp-todo-app/static/js/2.b7f2fb11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b249c2669a9c753d258",
+    "url": "/react-bootcamp-todo-app/static/js/main.3adb6c7c.chunk.js"
+  },
+  {
+    "revision": "4f175e16cd77edc30281",
+    "url": "/react-bootcamp-todo-app/static/js/runtime-main.cd33c183.js"
+  }
+]);
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { todoReducer } from "./reducers"
-import { combineReducers, createStore } from "redux"
-
-const reducers = combineReducers({
-    todoReducer: todoReducer
-
-})
-
-export const store = createStore(reducers)
