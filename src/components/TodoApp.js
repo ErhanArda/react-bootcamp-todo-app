8caa1b4cf2b8ffbc459c5f4f8026da8ce863d4de
Erhan Arda <erhanarda7@gmail.com>
2020-05-01 21:13:14 +0300

    updated

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -7,35 +7,7 @@ class TodoApp extends Component {
         // eslint-disable-next-line no-useless-constructor
         constructor(props) {
             super(props)
-
-            // this.state = {
-            //     todos: []
-            // }
         }
-
-        // addTodo = (content) => {
-        //     const newTodo = {
-        //         id: Math.random(),
-        //         completed: false,
-        //         content
-        //     }
-        //     this.setState({
-        //         todos: [...this.state.todos, newTodo],
-        //         value: ""
-        //     })
-        // }
-
-
-        // toggleTodo = (id) => {
-        //     this.setState({
-        //         todos: this.state.todos.map((todo) => {
-        //             if (todo.id === id) {
-        //                 return { ...todo, completed: !todo.completed }
-        //             }
-        //             return todo
-        //         })
-        //     })
-        // }
 
         render() {
             return (
@@ -50,5 +22,3 @@ class TodoApp extends Component {
 
 
 export default TodoApp
-
-
